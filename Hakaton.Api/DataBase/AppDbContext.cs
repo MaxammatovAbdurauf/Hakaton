dfdc742cc2a424f9a49123652622d4331ce87985
Maxammatov_Abdurauf <maxammatovabdurauftdyu@gmail.com>
2022-12-17 17:57:39 +0500

    I have added some entities

--- Hakaton.Api/DataBase/AppDbContext.cs
+++ Hakaton.Api/DataBase/AppDbContext.cs
@@ -9,5 +9,7 @@ public class AppDbContext : IdentityDbContext<User,Role,Guid>
 {
     public DbSet<Result>? Results { get; set; }
     public DbSet<Task>? Tasks { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<UserCourse> UserCourses { get; set; }
     public AppDbContext(DbContextOptions options) : base(options) { }
 }
---
+++ Hakaton.Api/DataBase/Migrations/20221217125628_someentities.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HakatonApi.Database.Migrations
+{
+    public partial class someentities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Tasks",
+                newName: "TaskName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaskName",
+                table: "Tasks",
+                newName: "Title");
+        }
+    }
+}
